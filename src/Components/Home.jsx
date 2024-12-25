fcbca5196d9f86b22c4e86ec9779d5d5321be5d4
siddharthswami23 <siddharth.swami@walchandsangli.ac.in>
2024-12-25 20:22:00 +0530

    wallpaper

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,14 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import SideNav from "./partials/SideNav";
 import TopNav from "./partials/TopNav";
+import axios from "../utils/Axios";
+import Header from "./partials/Header";
 
 const Home = () => {
   document.title = "SCSDB | HomePage";
+  const [WallPaper, setWallPaper] = useState(null);
+
+  const GetWallPaper = async () => {
+    try {
+      const response = await axios.get("/trending/all/day");
+      let randomimg =
+        response.data.results[
+          Math.floor(Math.random() * response.data.results.length)
+        ];
+      setWallPaper(randomimg);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // console.log(WallPaper);
+  useEffect(() => {
+    !WallPaper && GetWallPaper();
+  }, []);
+
   return (
     <div className="w-full h-full flex">
       <SideNav />
       <div className="w-[80%] h-full">
         <TopNav />
+        <Header data={WallPaper} />
       </div>
     </div>
   );
---
+++ src/Components/partials/Header.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Header = ({data}) => {
+    console.log(data);
+  return <div>Header</div>;
+};
+ 
+export default Header;
